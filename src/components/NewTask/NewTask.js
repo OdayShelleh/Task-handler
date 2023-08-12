32222b961840e64a2712941a88a1bc637fb23795
Oday Shelleh <shelleh2000@gmail.com>
2023-08-12 14:24:00 +0300

    Task app using custom hooks and conected with api

---
+++ src/components/NewTask/NewTask.js
@@ -0,0 +1,36 @@
+import useHttp from "../../hooks/use-http";
+
+import Section from "../UI/Section";
+import TaskForm from "./TaskForm";
+
+const NewTask = (props) => {
+  const { isLoading, error, sendRequest } = useHttp();
+  const createTask = (taskText, taskData) => {
+    const generatedId = taskData.name; // firebase-specific => "name" contains generated id
+    const createdTask = { id: generatedId, text: taskText };
+    props.onAddTask(createdTask);
+  };
+  const enterTaskHandler = async (taskText) => {
+    sendRequest(
+      {
+        url: "https://react-http-2c057-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json",
+
+        method: "POST",
+        body: { text: taskText },
+        headers: {
+          "Content-Type": "application/json",
+        },
+      },
+      createTask.bind(null, taskText)
+    );
+  };
+
+  return (
+    <Section>
+      <TaskForm onEnterTask={enterTaskHandler} loading={isLoading} />
+      {error && <p>{error}</p>}
+    </Section>
+  );
+};
+
+export default NewTask;
